a44e475490000bf9fc15ce7d69cdb222f65db779
Shashi Kumar <sulege@codeaurora.org>
2012-01-21 15:10:13 +0800

    Music: Fix to make progress bar update while suspend/resume
    - While playback, press fast forward continuously. Then press
      suspend and resume while still pressing FF. Release the
      fast forward button, the scroll bar moves back.
    - On resume the key down and up times are updated and the
      same values are used while calculating delta on last fast forward
      release. This give a wrong value and scroll bar moves back.
    - Changes are done to return from on key up event if seek
      time is in negative and seek flag is false.
    
    CRs-Fixed: 332155
    (cherry picked from commit 3d57e706411ada78f6729174d48e797a67d48380)
    
    Change-Id: I412582f332bcdef438077bf9794143627793c939

--- src/com/android/music/MediaPlaybackActivity.java
+++ src/com/android/music/MediaPlaybackActivity.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2007 The Android Open Source Project
+ * Copyright (c) 2012, Code Aurora Forum. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -812,6 +813,10 @@ public class MediaPlaybackActivity extends Activity implements MusicUtils.Defs,
                         break;
                     }
                     if (mService != null) {
+                        if (mStartSeekPos == -1 && mSeeking == false)
+                        {
+                            return true;
+                        }
                         if (!mSeeking && mStartSeekPos >= 0) {
                             mPauseButton.requestFocus();
                             if (mStartSeekPos < 1000) {
@@ -833,6 +838,10 @@ public class MediaPlaybackActivity extends Activity implements MusicUtils.Defs,
                         break;
                     }
                     if (mService != null) {
+                        if (mStartSeekPos == -1 && mSeeking == false)
+                        {
+                            return true;
+                        }
                         if (!mSeeking && mStartSeekPos >= 0) {
                             mPauseButton.requestFocus();
                             mService.next();
